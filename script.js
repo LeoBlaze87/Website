faeb12d1dd5eb182d2fdaa8b4c6c347f18a4d236
LeoBlaze87 <johnniewilliams87@gmail.com>
2021-03-28 23:07:16 -0400

    Basic

--- script.js
+++ script.js
@@ -2,3 +2,29 @@ const h2 = document.createElement('h2');
 h2.textContent = "This content added by JavaScript";
 
 document.querySelector('body').appendChild(h2);
+
+const personalInfo = {
+    firstName: 'Johnnie',
+    lastName: 'Williams',
+    company: {
+        name: 'Global Linking Solutions',
+        jobTitle: 'Technical Escalations Manager'
+    },
+    hobbies: {
+     'programming':'gaming','spending time with my family':
+    }
+}
+
+function deepIterator(target) {
+    if (Array.isArray(target)) {
+      for (const element of target) {
+        deepIterator(element);
+      }
+    } else if (typeof target === 'object') {
+      for (const key in target) {
+        deepIterator(target[key]);
+      }
+    } else {
+      console.log(target);
+    }
+}
