457fc0b3b31c7a71093be1f77dc1d68f00596808
LeoBlaze87 <johnniewilliams87@gmail.com>
2021-03-29 20:18:36 -0400

    Good

--- script.js
+++ script.js
@@ -2,18 +2,9 @@ const h2 = document.createElement('h2');
 h2.textContent = "This content added by JavaScript";
 
 document.querySelector('body').appendChild(h2);
+const clickerButton = document.getElementById("clicker")
 
-const personalInfo = {
-    firstName: 'Johnnie',
-    lastName: 'Williams',
-    company: {
-        name: 'Global Linking Solutions',
-        jobTitle: 'Technical Escalations Manager'
-    },
-    hobbies: {
-     'programming':'gaming','spending time with my family':
-    }
-  }
+
 function deepIterator(target) {
     if (Array.isArray(target)) {
       for (const element of target) {
